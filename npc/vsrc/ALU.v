54feb62f8d97e592d3db28d98fdf2a559cd5a17a
Freeaing <wangsnowy392@gmail.com>
2025-03-01 23:44:48 +0800

    second commit for ALU

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -1,22 +1,33 @@
 
 module  ALU(
-    input A,
-    input B,
-    input operator,
-    output carry,
-    output overflow,
-    output result,
-    output zero
+    input[3:0]     dA,
+    input[3:0]     dB,
+    input[3:0]     operator_sel,
+    output         carry,
+    output[3:0]    result,
+    output         zero
 );
-//add:000
+    reg[7:0] operate;
 
-//sub:001
+//operator | add | sub | not | and  | or | xor | ilt | iet
+    always @(operator_sel) begin
+        case(operator_sel)
+        4'b0000: operate <= 8'b10000000;
+        4'b0001: operate <= 8'b01000000; 
+        4'b0010: operate <= 8'b00100000;
+        4'b0011: operate <= 8'b00010000;
+        4'b0100: operate <= 8'b00001000;
+        4'b0101: operate <= 8'b00000100;
+        4'b0110: operate <= 8'b00000010;
+        4'b0111: operate <= 8'b00000001;
+        endcase
+    end
 
-//shift
+endmodule
 
-//compare
-
-//judge:overflow/C/O
-
-
-endmodule // ALU
+module inv(
+    input[3:0]  din,
+    output[3:0] din_inv
+);
+    assign din_inv = ~din + 32'd1;
+endmodule
