716f161441b93cdec8ea1931411895059fe70437
FreMux <fremux@ubuntu.myguest.virtualbox.org>
2025-03-01 21:21:20 +0800

    first commit!

---
+++ npc/vsrc/ALU.v
@@ -0,0 +1,22 @@
+
+module  ALU(
+    input A,
+    input B,
+    input operator,
+    output carry,
+    output overflow,
+    output result,
+    output zero
+);
+//add:000
+
+//sub:001
+
+//shift
+
+//compare
+
+//judge:overflow/C/O
+
+
+endmodule // ALU
